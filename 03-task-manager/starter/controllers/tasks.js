a32fbecc16ea35d86cf85af90108762eec54847c
Diariata Fall <diariata.f@outlook.com>
2023-11-07 14:02:21 +0100

    add models, the controllers and the front route in the back

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,23 +1,67 @@
 const Task = require("../models/Task");
-const getAllTasks = (req, res) => {
-  res.send("Get all Tasks");
-};
 
+//geting all the tasks
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find(req.body);
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+// create a task
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ message: `No task with id: ${req.params.id}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send("update Task");
+const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ message: `No task with id: ${req.params.id}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete Task");
+const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ msg: `No task with id : ${req.params.id}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 //exports as an object since we're going to add more
--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 //models representation of the collection
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide a name"],
+    trim: true, //remove whitespace from both ends of the string
+    maxlength: [20, " name cannot be more than 20 charachters"],
+  },
+  completed: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
